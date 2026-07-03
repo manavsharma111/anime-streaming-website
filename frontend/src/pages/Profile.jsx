824f741c0a1b096cc95fe981d6a1ccc276edc20f
manavsharma111 <manavsharma3825@gmail.com>
2026-07-03 21:21:48 +0530

    fix: lowercase 'slice' in authSlice import to resolve Vercel build error

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { checkAuth } from "../redux/Slice/authSlice"
+import { checkAuth } from "../redux/slice/authSlice"
 import {
   getWatchHistory,
   deleteAllHistory,
